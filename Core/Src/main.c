a02d630b0c37b9e85bc1d951929e420d395bf8c5
baozhenhao <3020708518@qq.com>
2026-04-20 20:59:23 +0800

    2026-04-20  点亮小灯，板子基础功能验证通过

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -87,7 +87,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
   /* USER CODE END 2 */
 
   /* Infinite loop */
